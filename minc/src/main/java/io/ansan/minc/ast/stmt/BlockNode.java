a80f6642b5b1fe49b97cb7baa4b30a69b1729a14
Anthony Anderson <anntony4122@gmail.com>
2022-09-22 05:24:48 -0400

    Fix compilation errors
    Signed-off-by: Anthony Anderson <anntony4122@gmail.com>

--- minc/src/main/java/io/ansan/minc/ast/stmt/BlockNode.java
+++ minc/src/main/java/io/ansan/minc/ast/stmt/BlockNode.java
@@ -1,9 +1,14 @@
 package io.ansan.minc.ast.stmt;
 
-import io.ansan.minc.ast.INode;
+import io.ansan.minc.ast.INode.IStmtNode;
 import io.ansan.minc.token.Token;
 
 import java.util.List;
 
-public record BlockNode(Token start, List<INode.IStmtNode> statements, Token end) implements INode.IStmtNode {
+public record BlockNode(Token start, List<IStmtNode> statements, Token end) implements IStmtNode {
+
+  @Override
+  public AstNode get_node_type() {
+    return AstNode.STATEMENT_BLOCK;
+  }
 }
--- minc/src/main/java/io/ansan/minc/ast/stmt/DeferNode.java
+++ minc/src/main/java/io/ansan/minc/ast/stmt/DeferNode.java
@@ -3,5 +3,11 @@ package io.ansan.minc.ast.stmt;
 import io.ansan.minc.ast.INode.IStmtNode;
 import io.ansan.minc.token.Token;
 
-public record DeferNode(Token ident, BlockNode nodes) implements IStmtNode {}
+public record DeferNode(Token ident, BlockNode nodes) implements IStmtNode {
+  
+  @Override
+  public AstNode get_node_type() {
+    return AstNode.STATEMENT_DEFER;
+  }
+}
 
